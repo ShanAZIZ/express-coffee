641f97de7b9a0169d1b327717c3783c891cfb8a9
Shan AZIZ <shan.azizh@gmail.com>
2022-04-01 15:31:42 +0200

    feat(auth): login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import {UserDocument, UserModel, UserProps} from "../models";
+import {SessionModel, UserDocument, UserModel, UserProps} from "../models";
 import {SecurityUtils} from "../utils";
 
 export class AuthService {
@@ -25,6 +25,25 @@ export class AuthService {
         })
 
         return model.save();
+    }
+
+    public async logIn(info: Pick<UserProps, 'login' | 'password'>, platform: string) {
+        const user = await UserModel.findOne({
+            login: info.login,
+            password: SecurityUtils.sha512(info.password)
+        }).exec();
+        if(user === null){
+            throw new Error('User not found');
+        }
+        const expirationDate = new Date(new Date().getTime() + 604_800_000);
+        const session = await SessionModel.create({
+            platform,
+            expiration: expirationDate,
+            user : user._id
+        })
+        user.session.push(session.id);
+        await user.save();
+        return session;
     }
 
 }
